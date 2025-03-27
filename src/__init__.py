266517c49cfc518ab0d1a4424b05edb2c792be93
mobius-crypt <mobiusndou@gmail.com>
2025-03-27 15:25:58 +0200

    removed config

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,7 @@ from src.config import Config
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('Config')
+    # app.config.from_object('Config')
 
     with app.app_context():
         from . import routes
