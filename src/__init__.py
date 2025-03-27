916d5af64b3933b137634d6541139982383b6e45
mobius-crypt <mobiusndou@gmail.com>
2025-03-27 15:23:46 +0200

    updated Config

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from config import Config
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
+    app.config.from_object('Config')
 
     with app.app_context():
         from . import routes
