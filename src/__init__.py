6ec711c5979f1a345e1273eede5b1a3178ca00a1
mobius-crypt <mobiusndou@gmail.com>
2025-03-28 09:21:50 +0200

    setup templates and static folders

--- src/__init__.py
+++ src/__init__.py
@@ -2,14 +2,15 @@ from flask import Flask
 from src.config import Config
 
 def create_app():
-    app = Flask(__name__)
-    # app.config.from_object('Config')
+    app = Flask(
+        __name__,
+        template_folder="src/templates",  # Set templates folder
+        static_folder="src/static"        # Set static folder
+    )
+    app.config.from_object(Config)  # Uncomment if needed
 
     with app.app_context():
-
         from src.routes import home_bp
         app.register_blueprint(home_bp)
-        return app
 
-
-    
+    return app
